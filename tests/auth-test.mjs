eb011bc30154166090fe6c4e2d0d86754ecd15e7
Markus Felten <Markus.Felten@gmx.de>
2019-11-23 00:55:02 +0100

    test: check against local running ldap server

---
+++ tests/auth-test.mjs
@@ -0,0 +1,28 @@
+import test from "ava";
+import { StandaloneServiceProvider } from "@kronos-integration/service";
+import { ServiceLDAP } from "../src/service-ldap.mjs";
+
+test("service-ldap auth", async t => {
+  const sp = new StandaloneServiceProvider();
+
+  const ldap = new ServiceLDAP(
+    {
+      url: "ldap://localhost:3389",
+      bindDN: "uid={{username}},ou=accounts,dc=example,dc=com",
+      entitlements: {
+        base: "ou=groups,dc=example,dc=com",
+        attribute: "cn",
+        scope: "sub",
+        filter:
+          "(&(objectclass=groupOfUniqueNames)(uniqueMember=uid={{username}},ou=accounts,dc=example,dc=com))"
+      }
+    },
+    sp
+  );
+
+  await ldap.start();
+
+  t.is(ldap.state, "running");
+
+  t.deepEqual(await ldap.authenticate({ username: 'user1', password: 'test' }), { username: 'user1', entitlements: new Set(['konsum']) });
+});
