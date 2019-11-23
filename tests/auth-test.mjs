8e88318165ac7b8d76f43576059c13773f323f72
Markus Felten <Markus.Felten@gmx.de>
2019-11-23 01:59:11 +0100

    test: check invalid credentials

--- tests/auth-test.mjs
+++ tests/auth-test.mjs
@@ -2,21 +2,23 @@ import test from "ava";
 import { StandaloneServiceProvider } from "@kronos-integration/service";
 import { ServiceLDAP } from "../src/service-ldap.mjs";
 
+
+const config = {
+  url: "ldap://localhost:3389",
+  bindDN: "uid={{username}},ou=accounts,dc=example,dc=com",
+  entitlements: {
+    base: "ou=groups,dc=example,dc=com",
+    attribute: "cn",
+    scope: "sub",
+    filter:
+      "(&(objectclass=groupOfUniqueNames)(uniqueMember=uid={{username}},ou=accounts,dc=example,dc=com))"
+  }
+};
+
 test("service-ldap auth ok", async t => {
   const sp = new StandaloneServiceProvider();
-
   const ldap = new ServiceLDAP(
-    {
-      url: "ldap://localhost:3389",
-      bindDN: "uid={{username}},ou=accounts,dc=example,dc=com",
-      entitlements: {
-        base: "ou=groups,dc=example,dc=com",
-        attribute: "cn",
-        scope: "sub",
-        filter:
-          "(&(objectclass=groupOfUniqueNames)(uniqueMember=uid={{username}},ou=accounts,dc=example,dc=com))"
-      }
-    },
+    config,
     sp
   );
 
@@ -27,21 +29,27 @@ test("service-ldap auth ok", async t => {
   t.deepEqual(await ldap.authenticate({ username: 'user1', password: 'test' }), { username: 'user1', entitlements: new Set(['konsum']) });
 });
 
+test("service-ldap auth invalid", async t => {
+  const sp = new StandaloneServiceProvider();
+  const ldap = new ServiceLDAP(
+    config,
+    sp
+  );
 
-test("service-ldap no bind", async t => {
+  await ldap.start();
+
+  t.is(ldap.state, "running");
+
+  t.deepEqual(await ldap.authenticate({ username: 'user1', password: 'invalid' }), { username: 'user1', entitlements: new Set() });
+});
+
+test("service-ldap wrong url", async t => {
   const sp = new StandaloneServiceProvider();
 
   const ldap = new ServiceLDAP(
     {
       url: "ldap://localhost:3388",
-      bindDN: "uid={{username}},ou=accounts,dc=example,dc=com",
-      entitlements: {
-        base: "ou=groups,dc=example,dc=com",
-        attribute: "cn",
-        scope: "sub",
-        filter:
-          "(&(objectclass=groupOfUniqueNames)(uniqueMember=uid={{username}},ou=accounts,dc=example,dc=com))"
-      }
+      ...config
     },
     sp
   );
