9ea8aa98baa2f6e68760c7ecea28ab9b2657aff1
Markus Felten <Markus.Felten@gmx.de>
2019-11-23 01:36:48 +0100

    test: check invalid url

--- tests/auth-test.mjs
+++ tests/auth-test.mjs
@@ -2,12 +2,38 @@ import test from "ava";
 import { StandaloneServiceProvider } from "@kronos-integration/service";
 import { ServiceLDAP } from "../src/service-ldap.mjs";
 
-test("service-ldap auth", async t => {
+test("service-ldap auth ok", async t => {
   const sp = new StandaloneServiceProvider();
 
   const ldap = new ServiceLDAP(
     {
       url: "ldap://localhost:3389",
+      bindDN: "uid={{username}},ou=accounts,dc=example,dc=com",
+      entitlements: {
+        base: "ou=groups,dc=example,dc=com",
+        attribute: "cn",
+        scope: "sub",
+        filter:
+          "(&(objectclass=groupOfUniqueNames)(uniqueMember=uid={{username}},ou=accounts,dc=example,dc=com))"
+      }
+    },
+    sp
+  );
+
+  await ldap.start();
+
+  t.is(ldap.state, "running");
+
+  t.deepEqual(await ldap.authenticate({ username: 'user1', password: 'test' }), { username: 'user1', entitlements: new Set(['konsum']) });
+});
+
+
+test("service-ldap no bind", async t => {
+  const sp = new StandaloneServiceProvider();
+
+  const ldap = new ServiceLDAP(
+    {
+      url: "ldap://localhost:3388",
       bindDN: "uid={{username}},ou=accounts,dc=example,dc=com",
       entitlements: {
         base: "ou=groups,dc=example,dc=com",
