6dad2dbcc706406ba5037c3d7dce26c458e2eccb
Markus Felten <Markus.Felten@gmx.de>
2019-12-04 12:37:53 +0100

    test: start-local-ldap suport for brew@darwin

--- tests/start-local-ldap.sh
+++ tests/start-local-ldap.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
 
-slapd -f tests/fixtures/ldap/slapd.conf -h ldap://localhost:3389 &
+mkdir -p /tmp/slapd
+
+case $(uname) in
+    "Darwin" )
+        export PATH="/usr/local/opt/openldap/bin:/usr/local/opt/openldap/sbin:$PATH"
+        SLAPD=/usr/local/opt/openldap/libexec/slapd
+        SLAPD_CONF=/tmp/slapd/slapd.conf
+        sed 's/\/etc\/ldap\//\/usr\/local\/etc\/openldap\//' tests/fixtures/ldap/slapd.conf > ${SLAPD_CONF}
+    ;;
+    
+    "*" )
+        SLAPD=slapd
+        SLAPD_CONF=tests/fixtures/ldap/slapd.conf
+    ;;
+esac
+
+
+${SLAPD} -f ${SLAPD_CONF} -h ldap://localhost:3389 -d 9 &
 ldapadd -h localhost:3389 -D cn=Manager,dc=example,dc=com -w test -f tests/fixtures/ldap/base.ldif
